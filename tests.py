2e10d7f5d62f88fefbc3ed60987f5933dc45abb9
Oskar Sharipov <oskar.sharipov [at] tuta.io>
2019-10-06 20:37:59 +0300

    Add test of is_command function

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import main
 
 
 class TestNewIndexMethod(unittest.TestCase):
- 
+
     def test_arrows(self):
         previous_index = 10
         strings, expected_results = ('<<', '>>>', '><>'), (8, 13, 10)
@@ -21,5 +21,16 @@ class TestNewIndexMethod(unittest.TestCase):
         for i in range(3):
             self.assertEqual(
                 main.new_index(strings[i], previous_index),
+                expected_results[i]
+            )
+
+class TestIsCommandMethod(unittest.TestCase):
+
+    def test_smoke(self):
+        strings, expected_results = ('123', '>>', '@7'), (False, True, True)
+
+        for i in range(3):
+            self.assertEqual(
+                main.is_command(strings[i]),
                 expected_results[i]
             )
