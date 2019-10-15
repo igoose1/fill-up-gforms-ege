4458b03396ff2c18d854cf7fc3ff6fb885d64cd1
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-10-15 10:48:37 +0300

    Move xpath strings to the top of code
    WIP #4

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ from time import sleep
 
 FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScyrlhh0IGxev3S9CKrO8vyX_MkhfLSKCl_2Ux3QZRDo2_DaA/viewform'
 driver = webdriver.WebKitGTK
+INPUT_FIELD_XPATH_LAYOUT = '/html/body/div/div[2]/form/div/div[2]/div[2]/div[{index}]/div/div[2]/div/div[1]/div/div[1]/input'
+SEND_BUTTON_XPATH = '/html/body/div/div[2]/form/div/div[2]/div[3]/div[1]/div/div/span'
+
+# New design xpaths. WIP (have no opportunity to look at new design page).
+# INPUT_FIELD_XPATH_LAYOUT = '/html/body/div/div[2]/form/div/div[2]/div[2]/div[{index}]/div/div[2]/div/div[1]/div/div[1]/input'
+# SEND_BUTTON_XPATH = '/html/body/div/div[2]/form/div/div[2]/div[3]/div[1]/div/div/span'
 
 # Stop editing lines.
 # ----------------------------
@@ -63,17 +69,16 @@ def preload(driver):
 def fill(driver, fill_with):
     ANSWER_FIELDS = 3
     fill_xpaths = [
-        '/html/body/div/div[2]/form/div/div[2]/div[2]/div[{ind}]/div/div[2]/div/div[1]/div/div[1]/input'.format(
-            ind=ind+1
+        INPUT_FIELD_XPATH_LAYOUT.format(
+            index=index+1
         )
-        for ind in range(ANSWER_FIELDS)
+        for index in range(ANSWER_FIELDS)
     ]
-    send_button_xpath = '/html/body/div/div[2]/form/div/div[2]/div[3]/div[1]/div/div/span'
 
     for i in range(ANSWER_FIELDS):
         element = driver.find_element_by_xpath(fill_xpaths[i])
         element.send_keys(fill_with[i])
-    send_button_element = driver.find_element_by_xpath(send_button_xpath)
+    send_button_element = driver.find_element_by_xpath(SEND_BUTTON_XPATH)
     send_button_element.click()
 
 
